3cf66014fd544d72d65e77e25ed5d73766a9f67e
Joan Rasmussen <raz.joan@gmail.com>
2022-01-14 10:47:46 -0700

    Add all Components folders and files

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const App = () => {
+    return (
+        <div className='body-container'>
+            <header>Header</header>
+            <main>Main</main>
+            <footer>Footer</footer>
+        </div>
+        )
+}
+
+export default App

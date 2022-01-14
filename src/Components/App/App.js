a0b56c56a69ea2a22e1555d8d90ce9c96fe296db
Joan Rasmussen <raz.joan@gmail.com>
2022-01-14 11:44:43 -0700

    Connect Sass files to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,10 @@
 import React from "react"
-
+import './App.scss'
 
 const App = () => {
     return (
         <div className='body-container'>
-            <header>Header</header>
+            <header><p>Header</p></header>
             <main>Main</main>
             <footer>Footer</footer>
         </div>

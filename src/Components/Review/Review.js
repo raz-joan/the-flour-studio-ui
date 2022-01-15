7d9d66ee41ac254d15c62b506320d3323c77f7dd
Joan Rasmussen <raz.joan@gmail.com>
2022-01-15 12:55:53 -0700

    Build and style Review

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -3,8 +3,9 @@ import './Review.scss'
 
 const Review = () => {
     return (
-        <div>
-            <p>Review connected</p>
+        <div className='customer-review'>
+            <h4 className='customer-review-title'>"CustomerName" gave this grain a rating of "Number" on "Date"</h4>
+            <p className='customer-review-note'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     )
 }

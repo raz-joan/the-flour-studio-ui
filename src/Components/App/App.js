5bb9f87c586e9aca2fd7b4017287405bcce5cab6
Joan Rasmussen <raz.joan@gmail.com>
2022-01-15 19:39:20 -0700

    Add useEffect & GETs for grains / reviews to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './App.scss'
 import Header from '../Header/Header'
 import Welcome from '../Welcome/Welcome'
@@ -7,13 +7,30 @@ import GrainCardsContainer from '../GrainCardsContainer/GrainCardsContainer'
 import SpecificGrainContainer from '../SpecificGrainContainer/SpecificGrainContainer'
 
 const App = () => {
+
+    const [ grains, setGrains ] = useState([])
+    const [ reviews, setReviews ] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/v1/grains')
+            .then(res => res.json())
+            .then(data => {
+                setGrains(data)
+            })
+        fetch('http://localhost:3000/api/v1/reviews')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
+    }, [])
+
     return (
         <div className='body-container'>
             <Header />
             <main>
                 <Welcome />
-                <GrainCardsContainer />
-                <SpecificGrainContainer />
+                {/* <GrainCardsContainer grains={ grains }/> */}
+                {/* <SpecificGrainContainer grains={ grains } reviews={ reviews }/> */}
             </main>
             {/* <Footer /> */}
         </div>

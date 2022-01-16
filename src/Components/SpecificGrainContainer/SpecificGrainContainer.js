90de7b0e529b4832c873fa0b76ecc6650675caff
Joan Rasmussen <raz.joan@gmail.com>
2022-01-16 08:48:50 -0700

    Add fetch for reviews to SpecificGrainContainer

--- src/Components/SpecificGrainContainer/SpecificGrainContainer.js
+++ src/Components/SpecificGrainContainer/SpecificGrainContainer.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './SpecificGrainContainer.scss'
 import SpecificGrainInfo from '../SpecificGrainInfo/SpecificGrainInfo'
 import ReviewForm from '../ReviewForm/ReviewForm'
 import ReviewContainer from '../ReviewContainer/ReviewContainer'
 
-const SpecificGrainContainer = ({ reviews }) => {
+const SpecificGrainContainer = () => {
+
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/v1/reviews')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
+    }, [])
+
     return (
         <div className='specific-grain-container'>
             <SpecificGrainInfo />

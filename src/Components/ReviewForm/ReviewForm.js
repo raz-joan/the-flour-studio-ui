2661c6e11efed8156d0efd09e93354afc0de6af9
Joan Rasmussen <raz.joan@gmail.com>
2022-01-17 14:23:15 -0700

    Fix spacing on ReviewForm

--- src/Components/ReviewForm/ReviewForm.js
+++ src/Components/ReviewForm/ReviewForm.js
@@ -42,19 +42,19 @@ const ReviewForm = ({ grainName, postNewReview }) => {
             <h3 className='review-form-title'>Leave a Review!</h3>
             <div className='review-form-label-input-container'>
                 <label className='review-label' htmlFor='customer-name'>The name that you would like to appear on the review: </label>
-                <input className='review-input' id='customer-name' type='text' value={userName} onChange={(event) => setUserName(event.target.value)}/>
+                <input className='review-input' id='customer-name' type='text' value={ userName } onChange={ (event) => setUserName(event.target.value) }/>
             </div>
             <div className='review-form-label-input-container'>
                 <label className='review-label' htmlFor='customer-rating'>Your rating from 1 to 5 (low to high): </label>
-                <input className='review-input' id='customer-rating' type='number' min='1' max='5' step='1' value={userRating} onChange={(event) => setUserRating(event.target.value)}/>
+                <input className='review-input' id='customer-rating' type='number' min='1' max='5' step='1' value={ userRating } onChange={ (event) => setUserRating(event.target.value) }/>
             </div>
             <div className='review-form-label-input-container'>
                 <label className='review-label' htmlFor='customer-note'>What would you like to share with other grain enthusiasts? </label>
-                <textarea className='review-text-area' id='customer-note' value={userNote} onChange={(event) => setUserNote(event.target.value)}></textarea>
+                <textarea className='review-text-area' id='customer-note' value={ userNote } onChange={ (event) => setUserNote(event.target.value) }></textarea>
             </div>
             <div className='review-button-container'>
-                <button className='review-form-button' onClick={(event) => submitReview(event) }>Submit my Review!</button>
-                { inputError && <p className='input-error-message'>All three questions require an answer to submit a review.</p>}
+                <button className='review-form-button' onClick={ (event) => submitReview(event) }>Submit my Review!</button>
+                { inputError && <p className='input-error-message'>All three questions require an answer to submit a review.</p> }
             </div>
         </form>
     )

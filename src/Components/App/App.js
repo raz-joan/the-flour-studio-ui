04f80afb117ec781baa2996ba119f682514b91a5
Joan Rasmussen <raz.joan@gmail.com>
2022-01-16 17:03:27 -0700

    Update App with error message if fetch fails

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,27 +11,34 @@ import apiCalls from "../../apiCalls"
 const App = () => {
 
     const [grains, setGrains] = useState([])
+    const [fetchError, setFetchError] = useState('')
 
     useEffect(() => {
-        // fetch('http://localhost:3000/api/v1/grains')
-        //     .then(res => res.json())
         apiCalls.getData('http://localhost:3000/api/v1/grains')
-            .then(data => setGrains(data))
+            .then(data => {
+                if (data[0]) {
+                    setGrains(data)
+                } else {
+                    setFetchError('Oops ... unable to connect to the server!')
+                }
+            })
     }, [])
 
     return (
         <div className='body-container'>
             <Header />
             <main>
+                { fetchError ? <p className='fetch-error-message'>{ fetchError }</p> :                
                 <Routes>
                     <Route path='/' element={ <Welcome /> }/>
-                    <Route path='/grains' element={<GrainCardsContainer grains={grains} />} />
-                    <Route path='/grains/:id' element={<SpecificGrainContainer />} />
+                    <Route path='/grains' element={ <GrainCardsContainer grains={ grains } /> } />
+                    <Route path='/grains/:id' element={ <SpecificGrainContainer /> } />
                 </Routes>
+                }
             </main>
             {/* <Footer /> */}
         </div>
-        )
+    )
 }
 
 export default App

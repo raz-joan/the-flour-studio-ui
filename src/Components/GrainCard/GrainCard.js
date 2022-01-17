09081444e28164136589afcd7c63bd1331864b6d
Joan Rasmussen <raz.joan@gmail.com>
2022-01-17 07:40:17 -0700

    Fix GrainCards colors for with and without gluten

--- src/Components/GrainCard/GrainCard.js
+++ src/Components/GrainCard/GrainCard.js
@@ -6,16 +6,18 @@ import './GrainCard.scss'
 const GrainCard = ({ grain }) => {
 
     const navigate = useNavigate()
+    let hasGlutenColor = grain.hasGluten ? 'blue' : 'gold'
     let inStockColor = grain.inStock ? 'green' : 'red'
+    let inStockText = grain.inStock ? 'In Stock' : 'Out of Stock'
 
     return (
         <article className='grain-card' key={ grain.id }>
-            <div className='color-circle'></div>
+            <div className={ `color-circle-${ hasGlutenColor }` }></div>
             <h2 className='grain-card-title'>{ grain.name }</h2>
             <p className='grain-card-info'>{ grain.classification }</p>
             <p className='grain-card-info'>${ grain.pricePerLb } / lb</p>
-            <p className='grain-card-info' className={inStockColor}>{grain.inStock ? 'In Stock' : 'Out of Stock'}</p>
-            <button className='grain-card-button' onClick={() => navigate(`/grains/${grain.id}`)}>See Grain Details</button>
+            <p className='grain-card-info' className={ inStockColor }>{ inStockText }</p>
+            <button className='grain-card-button' onClick={ () => navigate(`/grains/${ grain.id }`) }>See Grain Details</button>
         </article>
     )
 }
